1125c49634a6f85a8673e4d424282c0d07ad3901
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-03-30 21:53:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,11 @@ bot.on("message", async message => {
   if (message.content === '!help') {
     return message.channel.send(`***Comming Soon...***`)
   }
+  if (message.content === '!random') {
+    let towers = ["**<@413984212119715840>'s** channel: https://www.youtube.com/channel/UCy_KxAueZjIGafQ62_5J1sQ", "**<@226658795189698561>'s** channel: https://www.youtube.com/channel/UCMHmzeE7ssaO0fqJZfovAbw", "**<@346687165868015616>'s** channel: https://www.youtube.com/c/HalfBakedGaming15", "**<@125507197584146432>'s** channel: https://www.youtube.com/user/p0nchok1", "**<@418071433734914070>'s** channel: https://www.youtube.com/confusinq"]
+    let choice = Math.floor((Math.random() * towers.length));
+    return message.channel.send(`<@${message.author.id}>, random spin:\n${towers[choice]}!`)
+  }
 });
 
 bot.login(process.env.BOT_TOKEN);
