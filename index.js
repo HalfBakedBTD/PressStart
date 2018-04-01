b83c896d5ad1bbcffa6e6ea1c1a89c4da58b051e
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-01 19:31:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function shout(bot) {
 }
 
 function store(bot) {	
-	  bot.channels.filter(c => c.name === 'moneys').forEach(channel => channel.send(`UPDATE`).setAttachment(file, "./xp.json"));
+	  bot.channels.filter(c => c.name === 'moneys').forEach(channel => channel.send(`UPDATE`).setAttachment("./xp.json"));
   setTimeout(() => store(bot), 1*30000);
 }
 
