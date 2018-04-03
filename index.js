7fb0034b3b2529849f456f3c50841f004cb97032
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-02 23:22:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
-
+const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+
+});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-
-  //bot.user.setActivity("tutorials on TSC", {type: "WATCHING"});
 
   bot.user.setGame("r!help");
 });
@@ -20,35 +37,9 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
   
-  if(cmd === `${prefix}kick`){
-
-    //!kick @daeshan askin for it
-
-    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!kUser) return message.channel.send("Can't find user!");
-    let kReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
-    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
-
-    //let kickEmbed = new Discord.RichEmbed()
-    //.setDescription("Kick")
-    //.setColor("#e56b00")
-    //.addField("Kicked User", `${kUser} with ID ${kUser.id}`)
-    //.addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
-    //.addField("Kicked In", message.channel)
-    //.addField("Tiime", message.createdAt)
-    //.addField("Reason", kReason);
-   
-
-    let kickChannel = message.guild.channels.find(`name`, "logs");
-    if(!kickChannel) return message.channel.send("Can't find logs channel.");
-
-    message.guild.member(kUser).kick(kReason);
-    kickChannel.send(`👢 USER KICKED 👢\n\nKicked: <@${kUser.id}>\n\nPunisher: <@${message.author.id}>\n\nKicked In: ${message.channel}\n\nReason: ${kReason}`);
-
-    return;
-  }
-
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+  
   if(cmd === `${prefix}ban`){
 
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
