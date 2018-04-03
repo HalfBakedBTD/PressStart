0e91cbdc28b0d65afd001d9bfbf1ad45a2dfd905
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-02 22:45:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,20 +30,21 @@ bot.on("message", async message => {
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
     if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
 
-    let kickEmbed = new Discord.RichEmbed()
-    .setDescription("~Kick~")
-    .setColor("#e56b00")
-    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
-    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
-    .addField("Kicked In", message.channel)
-    .addField("Tiime", message.createdAt)
-    .addField("Reason", kReason);
+    //let kickEmbed = new Discord.RichEmbed()
+    //.setDescription("Kick")
+    //.setColor("#e56b00")
+    //.addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+    //.addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+    //.addField("Kicked In", message.channel)
+    //.addField("Tiime", message.createdAt)
+    //.addField("Reason", kReason);
+   
 
     let kickChannel = message.guild.channels.find(`name`, "logs");
     if(!kickChannel) return message.channel.send("Can't find logs channel.");
 
     message.guild.member(kUser).kick(kReason);
-    kickChannel.send(kickEmbed);
+    kickChannel.send(`👢 USER KICKED 👢\n\nKicked: <@${kUser.id}>\n\nPunisher: <@${message.author.id}>\n\nKicked In: ${message.channel}\n\nReason: ${kReason}`);
 
     return;
   }
@@ -54,51 +55,53 @@ bot.on("message", async message => {
     if(!bUser) return message.channel.send("Can't find user!");
     let bReason = args.join(" ").slice(22);
     if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("No can do pal!");
-    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be banned!");
 
-    let banEmbed = new Discord.RichEmbed()
-    .setDescription("~Ban~")
-    .setColor("#bc0000")
-    .addField("Banned User", `${bUser} with ID ${bUser.id}`)
-    .addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
-    .addField("Banned In", message.channel)
-    .addField("Time", message.createdAt)
-    .addField("Reason", bReason);
+    //let banEmbed = new Discord.RichEmbed()
+    //.setDescription("~Ban~")
+    //.setColor("#bc0000")
+    //.addField("Banned User", `${bUser} with ID ${bUser.id}`)
+    //.addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
+    //.addField("Banned In", message.channel)
+    //.addField("Time", message.createdAt)
+    //.addField("Reason", bReason);
 
     let incidentchannel = message.guild.channels.find(`name`, "logs");
     if(!incidentchannel) return message.channel.send("Can't find logs channel.");
 
     message.guild.member(bUser).ban(bReason);
-    incidentchannel.send(banEmbed);
+    incidentchannel.send(`\🔨 USER BANNED \🔨\n\nBanned User: <@${bUser.id}>\n\nBanned By: <@${message.author.id}>\n\nBanned In: ${message.channel}\n\nReason: ${bReason}`);
 
 
     return;
   }
 
 
-  if(cmd === `${prefix}report`){
+  if(cmd === `${prefix}warn`){
 
     //!report @ned this is the reason
 
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!rUser) return message.channel.send("Couldn't find user.");
     let rreason = args.join(" ").slice(22);
+    if (!rreason) return message.channel.send(`Please supply a reason.`)
 
-    let reportEmbed = new Discord.RichEmbed()
-    .setDescription("Reports")
-    .setColor("#15f153")
-    .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
-    .addField("Reported By", `${message.author} with ID: ${message.author.id}`)
-    .addField("Channel", message.channel)
-    .addField("Time", message.createdAt)
-    .addField("Reason", rreason);
+    //let reportEmbed = new Discord.RichEmbed()
+    //.setDescription("Reports")
+    //.setColor("#15f153")
+    //.addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+    //.addField("Reported By", `${message.author} with ID: ${message.author.id}`)
+    //.addField("Channel", message.channel)
+    //.addField("Time", message.createdAt)
+    //.addField("Reason", rreason);
 
     let reportschannel = message.guild.channels.find(`name`, "logs");
     if(!reportschannel) return message.channel.send("Couldn't find logs channel.");
 
 
     message.delete().catch(O_o=>{});
-    reportschannel.send(reportEmbed);
+    rUser.send(`You were warned in ${message.guild.name} by ${message.author.username} for ${rreason}.`)
+    reportschannel.send(`⚠ WARNING ⚠\n\nWarned: <@${rUser.id}>\n\nWarned By: <@${message.author.id}>\n\nWarned In: ${message.channel}\n\nReason: ${rreason}`);
 
     return;
   }
