8b114d8385b184dc3f505614a0dea5c90470c1f6
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-02 23:47:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -40,29 +40,6 @@ bot.on("message", async message => {
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(bot,message,args);
 
-
-  
-
-
-
-
-  if(cmd === `${prefix}sinfo`){
-
-    //let sicon = message.guild.iconURL;
-    //let serverembed = new Discord.RichEmbed()
-    //.setDescription("Server Information")
-    //.setColor("#15f153")
-    //.setThumbnail(sicon)
-    //.addField("Server Name", message.guild.name)
-    //.addField("Created On", message.guild.createdAt)
-    //.addField("You Joined", message.member.joinedAt)
-    //.addField("Total Members", message.guild.memberCount);
-
-    return message.channel.send(`📟 Server Information 📟\n\nServer Name: ${message.guild.name}\n\nTotal Members: ${message.guild.memberCount}`);
-  }
-
-
-
   if(cmd === `${prefix}binfo`){
 
     let botAvatar = bot.user.displayAvatarURL;
