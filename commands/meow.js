73f847995a1a57aea4ee379082da279d037b2236
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-03 02:27:14 -0400

    Update meow.js

--- commands/meow.js
+++ commands/meow.js
@@ -4,9 +4,14 @@ const superAgent = require("superagent");
 exports.run = async (bot, message, args) => {
 
     let{body} = await superAgent
-    .get(`https://random.cat`);
+    .get(`https://random.cat/meow`);
 
-    message.channel.send(`Meow! This is a cat!\n${body.url}`);
+    let dogEmbed = new Discord.RichEmbed()
+    .setColor("#bd9a82")
+    .setTitle("Meow! This is a cat! :dog:")
+    .setImage(body.file);
+
+    message.channel.send(`${dogEmbed}`);
 }
 
 module.exports.help = {
