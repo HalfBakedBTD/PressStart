2530098e43bb52fdd8726e92d700db5cdb019bcd
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-02 23:33:42 -0400

    Create ban.js

---
+++ commands/ban.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    if(cmd === `${prefix}ban`){
+
+    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!bUser) return message.channel.send("Can't find user!");
+    let bReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("No can do pal!");
+    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be banned!");
+
+    //let banEmbed = new Discord.RichEmbed()
+    //.setDescription("~Ban~")
+    //.setColor("#bc0000")
+    //.addField("Banned User", `${bUser} with ID ${bUser.id}`)
+    //.addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
+    //.addField("Banned In", message.channel)
+    //.addField("Time", message.createdAt)
+    //.addField("Reason", bReason);
+
+    let incidentchannel = message.guild.channels.find(`name`, "logs");
+    if(!incidentchannel) return message.channel.send("Can't find logs channel.");
+
+    message.guild.member(bUser).ban(bReason);
+    incidentchannel.send(`\🔨 USER BANNED \🔨\n\nBanned User: <@${bUser.id}>\n\nBanned By: <@${message.author.id}>\n\nBanned In: ${message.channel}\n\nReason: ${bReason}`);
+
+
+    return;
+}
+
+module.exports.help = {
+  name: "ban"
+}
