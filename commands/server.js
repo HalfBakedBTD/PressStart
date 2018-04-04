853293d87646c634615ce4e992623310b4e24a4c
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-03 23:11:11 -0400

    Create server.js

---
+++ commands/server.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  message.reply(`here is my server: https://discord.gg/M5uaJQy`)
+}
+
+module.exports.help = {
+  name: "server"
+}
