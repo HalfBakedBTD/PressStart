8f8d334292cb482bf85df0166254e1cb889d848d
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-03 22:37:15 -0400

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 exports.run = async (bot, message, args) => {
-  client.generateInvite(['ADMINISTRATOR'])
+  bot.generateInvite(['ADMINISTRATOR'])
     .then(link => {
       message.reply(`Add me to your server: ${link}`);
     });
