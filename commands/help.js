a3a8e6f4cd0b979a8dd4670a0aaf637e73233c82
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-03 22:38:17 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 exports.run = async (bot, message, args) => {
-  message.reply(`here are my commands:\n\n📄 **help** - shows help message.`)
+  message.reply(`here are my commands:\n\n📄 **help** - shows help message.\n🗃 **sinfo** - gives you server info.\n🤖 **binfo** - gives you bot info.\n\n🔨 **ban** - bans a user in the server.`)
 }
 
 module.exports.help = {
