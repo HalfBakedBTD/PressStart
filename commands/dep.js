8d94a4a8cac3d6869b9e7bc9e629fd78370a8c15
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-04 12:08:16 -0400

    Update dep.js

--- commands/dep.js
+++ commands/dep.js
@@ -7,17 +7,17 @@ module.exports.run = async (bot, message, args) => {
     message.reply(`you have no coins to deposit!`)
   }
   
-  if(isNaN(args[1])) return message.reply("please supply a number! (`dep 10`)");
+  if(isNaN(args[0])) return message.reply("please supply a number! (`dep 10`)");
   
   let uCoins = coins[message.author.id].coins;
   let uBank = coins[message.author.id].bank;
   
-  if (parseInt(args[1]) > uCoins) return message.reply(`you don't ${parseInt(args[1])} coins to deposit`)
-  if (parseInt(args[1]) < 100) return message.repy(`you have to deposit more than 100 coins at a time.`)
+  if (parseInt(args[0]) > uCoins) return message.reply(`you don't ${parseInt(args[1])} coins to deposit`)
+  if (parseInt(args[0]) < 100) return message.repy(`you have to deposit more than 100 coins at a time.`)
   
   coins[message.author.id] = {
-    coins: sCoins - parseInt(args[1]),
-    bank: uBank + parseInt(args[1])
+    coins: uCoins - parseInt(args[0]),
+    bank: uBank + parseInt(args[0])
   };  
   message.reply(`you have deposited ${parseInt(args[1])} coins!`)
 }
