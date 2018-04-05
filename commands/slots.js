e5313185a0cfb2a33f36af4977281651212e241b
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-04 21:40:31 -0400

    Create slots.js

---
+++ commands/slots.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+let iumics = require("../money.json");
+const fs = require("fs");
+
+exports.run = async (bot, message, args) => {
+  if (!coins[message.author.id]) {
+    message.reply(`you have no coins to bet!`)
+  }
+  
+  if(isNaN(args[0])) return message.reply("please supply a number! (`bet 100`)");
+  
+  let bet = parseInt(args[0]);
+  let uCoins = coins[message.author.id].coins;
+  let uBank = coins[message.author.id].bank;
+  
+  let slots = ["🍎", "🍐", "💎", "🥝", "🍇", "🍓", "🍅", "🍋", "🍌", "🍎", "🍐", "🥝", "🍇", "🍓", "🍅", "🍋", "🍌"]
+  let a = Math.floor((Math.random() * slots.length));
+  let b = Math.floor((Math.random() * slots.length));
+  let c = Math.floor((Math.random() * slots.length));
+  let d = Math.floor((Math.random() * slots.length));
+  let e = Math.floor((Math.random() * slots.length));
+  let f = Math.floor((Math.random() * slots.length));
+  let g = Math.floor((Math.random() * slots.length));
+  let h = Math.floor((Math.random() * slots.length));
+  let i = Math.floor((Math.random() * slots.length));
+  message.reply(`**[-- SLOTS --]**\n${a[slots]}${b[slots]}${c[slots]}\n${d[slots]}${e[slots]}${f[slots]}\n${g[slots]}${h[slots]}${i[slots]}\n**[-- SLOTS --]**`)
+}
+
+module.exports.help = {
+  name: "slots"
+}
