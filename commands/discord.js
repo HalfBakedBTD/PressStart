4a5069fe7bfc1d1a0c8463b6a1f286247b163a87
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-05 21:31:34 -0400

    Update and rename server.js to discord.js

--- commands/discord.js
+++ commands/discord.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  message.reply(`here is my server: https://discord.gg/M5uaJQy`)
+  message.reply(`here is my discord: https://discord.gg/j3vmbja`)
 }
 
 module.exports.help = {
-  name: "server"
+  name: "discord"
 }
