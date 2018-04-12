a91c8981dee196d376920715c0ddcf6350a81ad2
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-11 22:04:42 -0400

    Create claim.js

---
+++ commands/claim.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+let coins = require("../coins.json");
+let claims = require("../claims.json")
+
+module.exports.run = async (bot, message, args) => {
+  if(!coins[message.author.id]){
+    coins[message.author.id] = {
+      coins: 0,
+      bank: 200
+    };
+  }
+  if(!claims[message.author.id]) {
+    claims[message.author.id] = {
+      claims: 0
+    };
+  }
+  
+  let uClaims = claims[message.author.id].claims + 1;
+  let check = '☑' * uClaims;
+  let uCoins = coins[message.author.id].coins;
+  let uBank = coins[message.author.id].bank;
+  let prize = uClaims * 50;
+  
+  claims[message.author.id] = {
+    claims: uClaims
+  }
+  coins[message.author.id] = {
+    coins: uCoins + prize,
+    bank: uBank
+  }
+  
+  message.reply("You have claimed ${prize} coins!\n\nStreak: ${uClaims}\n${check}")
+}
+
+module.exports.help = {
+  name: "claim"
+}
