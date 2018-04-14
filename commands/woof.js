a1b58e7b59457d8ab3cd20864d497e904dad9815
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-13 22:47:24 -0400

    Update woof.js

--- commands/woof.js
+++ commands/woof.js
@@ -6,7 +6,13 @@ exports.run = async (bot, message, args) => {
     let{body} = await superAgent
     .get(`https://random.dog/woof.json`);
 
-    message.channel.send(`Woof! This is a dog!\n${body.url}`);
+    //message.channel.send(`Woof! This is a dog!\n${body.url}`);
+    let dogEmbed = new Discord.RichEmbed()
+    .setColor('#000000')
+    .setThumbnail("https://tse3.mm.bing.net/th?id=OIP.QbYo3zcLpLvozS5IyOsKSAHaId&pid=15.1&P=0&w=300&h=300")
+    .addField("Woof! This is a dog!", `${body.url}`, true);
+  
+  message.channel.send(dogEmbed);
 }
 
 module.exports.help = {
