b93caec895b895ad271d93d79897eb08de8df38d
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-14 01:12:04 -0400

    Update meow.js

--- commands/meow.js
+++ commands/meow.js
@@ -6,7 +6,12 @@ exports.run = async (bot, message, args) => {
     let{body} = await superAgent
     .get(`https://aws.random.cat/meow`);
 
-    message.channel.send(`Meow! This is a cat!\n${body.file}`);
+    //message.channel.send(`Meow! This is a cat!\n${body.file}`);
+    let catEmbed = new Discord.RichEmbed()
+    .setColor('#2ecc71')
+    .setImage(body.file);
+  
+  message.channel.send(catEmbed);
 }
 
 module.exports.help = {
