82d681027f9bea915cbf16455e8d8096010f2524
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-14 12:27:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ bot.on('guildCreate', guild => {
     let eventEmbed = new Discord.RichEmbed()
     .setColor('#27ae60')
     .setThumbnail(botAvatar)
-    .setDescription(`**Event:** added to a guild.\n\n**Guild:** ${guild.name}\n**Owner**: ${guild.owner}\n**Owner ID:** ${guild.ownerID}\n**Members:** ${guild.memberCount}\nRegion: ${guild.region}\nVerification Level: ${guild.verificationLevel}`);
+    .setDescription(`**Event:** added to a guild.\n\n**Guild:** ${guild.name}\n**Owner**: ${guild.owner}\n**Owner ID:** ${guild.ownerID}\n**Members:** ${guild.memberCount}\n**Region:** ${guild.region}\n**Verification Level:** ${guild.verificationLevel}`);
   
    channel.send(eventEmbed);
   });
@@ -49,7 +49,7 @@ bot.on('guildDelete', guild => {
     let eventEmbed = new Discord.RichEmbed()
     .setColor('#e74c3c')
     .setThumbnail(botAvatar)
-    .setDescription(`**Event:** removed from a guild.\n\n**Guild:** ${guild.name}\n**Owner**: ${guild.owner}\n**Owner ID:** ${guild.ownerID}\n**Members:** ${guild.memberCount}\nRegion: ${guild.region}\nVerification Level: ${guild.verificationLevel}`);
+    .setDescription(`**Event:** removed from a guild.\n\n**Guild:** ${guild.name}\n**Owner**: ${guild.owner}\n**Owner ID:** ${guild.ownerID}\n**Members:** ${guild.memberCount}\n**Region:** ${guild.region}\n**Verification Level:** ${guild.verificationLevel}`);
   
    channel.send(eventEmbed);
   });
