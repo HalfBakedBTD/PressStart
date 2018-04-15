8c4d7d55ec564e5531eb719d4fc091c22464801e
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-15 13:46:43 -0400

    Update bal.js

--- commands/bal.js
+++ commands/bal.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 let coins = require("../coins.json");
+let xp = require("../xp.json");
 
 module.exports.run = async (bot, message, args) => {
   let cUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
@@ -10,15 +11,21 @@ module.exports.run = async (bot, message, args) => {
         bank: 200
       };
     }
+    if(!xp[message.author.id]){
+      xp[message.author.id] = {
+        xp: 0,
+        level: 1
+      };
+    }
 
     let uCoins = coins[message.author.id].coins;
     let uBank = coins[message.author.id].bank;
-    let uNet = uBank + uCoins;
+    let uNet = (uBank + uCoins) * xp[message.author.id].level;
 
     //return message.reply(`you have:\n\n💰 Coins: ${uCoins}\n\n🏦 Bank: ${uBank}\n\n⛹ Net Worth: ${uNet}`)
     let balEmbed = new Discord.RichEmbed()
     .setColor('#2ecc71')
-    .setDescription(`**<@${message.author.id}> has:\n\nCoins:** ${uCoins}\n**Bank:** ${uBank}\n\n**Net Worth:** ${uNet}`)
+    .setDescription(`<@${message.author.id}>, you have **${uCoins}** coins and **${uBank}** coins in the bank. Your net worth is **${uNet}**.`)
     
     return message.channel.send(balEmbed)
   }
@@ -28,15 +35,21 @@ module.exports.run = async (bot, message, args) => {
       bank: 200
     }
   }
+  if(!xp[cUser.id]){
+     xp[cUser.id] = {
+       xp: 0,
+       level: 1
+    };
+  }
 
   let plCoins = coins[cUser.id].coins;
   let plBank = coins[cUser.id].bank;
-  let plNet = plBank + plCoins;
+  let plNet = (plBank + plCoins) * xp[cUser.id].level;
 
   //return message.reply(`**${cUser}** has:\n\n💰 Coins: ${plCoins}\n\n🏦 Bank: ${plBank}\n\n⛹ Net Worth: ${plNet}`)
     let balEmbed = new Discord.RichEmbed()
     .setColor('#2ecc71')
-    .setDescription(`**${cUser} has:\n\nCoins:** ${plCoins}\n**Bank:** ${plBank}\n\n**Net Worth:** ${plNet}`)
+    .setDescription(`**${cUser} has **${plCoins}** coins and **${plBank}** coins in the bank. Their net worth is **${plNet}**.`)
     
     return message.channel.send(balEmbed)
 }
