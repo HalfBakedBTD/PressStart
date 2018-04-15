d4dfc6e9080ed8a8db2744195113443ca9e1f834
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-15 17:30:33 -0400

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -26,7 +26,7 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("🔨 USER BANNED 🔨")
     .setThumbnail("https://tse4.mm.bing.net/th?id=OIP.j3VvIndtHgqfynt9b8DwegHaHa&pid=15.1&P=0&w=300&h=300")
     .setColor("#2f3640")
-    .setDescription("**Banned User:** ${bUser}\n**Banned By:** <@${message.author.id}>\n**Banned In:** ${message.channel}\n**Reason:** ${bReason}");
+    .setDescription(`**Banned User:** ${bUser}\n**Banned By:** <@${message.author.id}>\n**Banned In:** ${message.channel}\n**Reason:** ${bReason}`);
     
     incidentchannel.send(banEmbed)
     return;
