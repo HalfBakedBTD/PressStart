4c274f5433d1bf600f7c4048cd505f1eaca1b85e
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-15 19:22:27 -0400

    Update top.js

--- commands/top.js
+++ commands/top.js
@@ -3,7 +3,32 @@ let coins = require("../coins.json");
 let xp = require("../xp.json");
 
 module.exports.run = async (bot, message, args) => {
-  let top = 1;
+  message.channel.send(`Scanning for the top user!`)
+  let topPlayer = message.author.id;
+  if(!coin[message.author.id]){
+    coins[message.author.id] = {
+      coins: 0,
+      bank: 200
+    };
+  }
+  let top = coins[message.author.id].coins + coins[message.author.id].bank;
+  bot.users.filter(u => u.id !== '434745565369073664').forEach(user => {
+    if(!coin[user.id]){
+      coins[user.id] = {
+        coins: 0,
+        bank: 200
+      };
+    }
+    let plBal = coins[user.id].coins + coins[user.id].bank;
+    if (plBal > top) {
+      top = plBal
+      topPlayer = user.id
+    }
+  });
+  let topEmbed = new Discord.RichEmbed()
+  .setDescription(`<@${topPlayer}> is the top player!`)
+
+  message.channel.send(topEmbed)
 }
 
 module.exports.help = {
