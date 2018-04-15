cf02d786b1e173c81e68e8f8fa4c3d41f9db3d31
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-15 19:07:33 -0400

    Create top.js

---
+++ commands/top.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+let coins = require("../coins.json");
+let xp = require("../xp.json");
+
+module.exports.run = async (bot, message, args) => {
+  let top = 1;
+}
+
+module.exports.help = {
+  name: "top"
+}
