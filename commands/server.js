a195c6eb6d2fe808eae5555369636df3625681d7
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-15 23:26:45 -0400

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -9,22 +9,29 @@ module.exports.run = async (bot, message, args) => {
         voice_channels += 1;
       }
     });
+    
     //message.reply(`here is the server information \n\n📟 Server Name: ${message.guild.name}\n\n👑 Owner: ${message.guild.owner}\n\n💳 Owner ID: ${message.guild.ownerID}\n\n🌐 Region: ${message.guild.region}\n\n🚦 Verification Level: ${message.guild.verificationLevel}\n\nTotal Members: ${message.guild.memberCount}`);
-    let guildAvatar = message.guild.iconURL;
-    let guildEmbed = new Discord.RichEmbed()
-    .setTitle("Server Information.")
-    .setColor('#27ae60')
-    .setThumbnail(guildAvatar)
-    .addField("Name:", `${message.guild.name}`, true)
-    .addField("Owner:", `${message.guild.owner}`, true)
-    .addField("Owner ID:", `${message.guild.ownerID}`, true)
-    .addField("Text Channels:", `${text_channels}`, true)
-    .addField("Voice Channels:", `${voice_channels}`, true)
-    .addField("Region:", `${message.guild.region}`, true)
-    .addField("Verification Level:", `${message.guild.verificationLevel}`, true)
-    .addField("Members:", `${message.guild.memberCount}`, true)
+    message.guild.fetchBans()
+      .then(bans => {
+        let guildAvatar = message.guild.iconURL;
+        let guildEmbed = new Discord.RichEmbed()
+        .setTitle("Server Information.")
+        .setColor('#27ae60')
+        .setThumbnail(guildAvatar)
+        .addField("Name:", `${message.guild.name}`, true)
+        .addField("Owner:", `${message.guild.owner}`, true)
+        .addField("Owner ID:", `${message.guild.ownerID}`, true)
+        .addField("Text Channels:", `${text_channels}`, true)
+        .addField("Voice Channels:", `${voice_channels}`, true)
+        .addField("Region:", `${message.guild.region}`, true)
+        .addField("Verification Level:", `${message.guild.verificationLevel}`, true)
+        .addField("Members:", `${message.guild.memberCount}`, true)
+        .addField("Bans Size:", `${bans.size}`, true)
     
     message.channel.send(guildEmbed)
+
+    .catch(console.error);
+  }
 }
 
 module.exports.help = {
