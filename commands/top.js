35c0fcb440ddac697f4f04036d23459fef03c722
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-04-15 20:11:29 -0400

    Update top.js

--- commands/top.js
+++ commands/top.js
@@ -4,14 +4,24 @@ let xp = require("../xp.json");
 
 module.exports.run = async (bot, message, args) => {
   message.channel.send(`Scanning for the top user!`)
-  let topPlayer = message.author.id;
+  let topCoins = `<@${message.author.id}>`;
+  let topXP = `<@${message.author.id}>`;
+  let topLevel = `<@${message.author.id}>`;
   if(!coins[message.author.id]){
     coins[message.author.id] = {
       coins: 0,
       bank: 200
     };
   }
-  let top = coins[message.author.id].coins + coins[message.author.id].bank;
+  if(!xp[message.author.id]){
+    xp[message.author.id] = {
+      xp: 0,
+      level: 1
+    };
+  }
+  let topCoinAmt = coins[message.author.id].coins + coins[message.author.id].bank;
+  let topXPAmt = xp[message.author.id].xp;
+  let topLevelAmt = xp[message.author.id].level;
   bot.users.filter(u => u.id !== `${message.author.id}`).forEach(user => {
     if(!coins[user.id]){
       coins[user.id] = {
@@ -19,14 +29,46 @@ module.exports.run = async (bot, message, args) => {
         bank: 200
       };
     }
+    if(!xp[message.author.id]){
+      xp[message.author.id] = {
+        xp: 0,
+        level: 1
+      };
+    }
     let plBal = coins[user.id].coins + coins[user.id].bank;
-    if (plBal > top) {
-      top = plBal
-      topPlayer = user.id
+    let plXP = xp[message.author.id].xp;
+    let plLevel = xp[message.author.id].level;
+    if (plBal > topCoinAmt) {
+      topCoinAmt = plBal
+      topCoins = `<@${user.id}>`
+    }
+    if (plBal === topCoinAmt) {
+      topCoinAmt = plBal
+      topCoins = topCoins + `and <@${user.id}>`
+    }
+    if (plBal > topCoinAmt) {
+      topCoinAmt = plBal
+      topCoins = `<@${user.id}>`
+    }
+    if (plXP > topXPAmt) {
+      topXPAmt = plXP
+      topXP = `<@${user.id}>`
+    }
+    if (plXP === topXPAmt) {
+      topXPAmt = plXP
+      topXP = topXP + `and <@${user.id}>`
+    }
+    if (plLevel > topLevelAmt) {
+      topLevelAmt = plLevel
+      topLevel = `<@${user.id}>`
+    }
+    if (plLevel > topLevelAmt) {
+      topLevelAmt = plLevel
+      topLevel = topLevel + `and <@${user.id}>`
     }
   });
   let topEmbed = new Discord.RichEmbed()
-  .setDescription(`<@${topPlayer}> is the top player!`)
+  .setDescription(`${topCoins} has the most coins. [💰${topCoinAmt}]\n${topXP} has the most xp. [${topXPAmt}XP]\n${topLevel} owns the current highest level. [Lvl. ${topLevelAmt}]`)
 
   message.channel.send(topEmbed)
 }
